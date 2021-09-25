46501ceef10325c902f036cac16f0ec58ec15019
S73ph4n <stephanpissot@gmail.com>
2021-09-25 17:21:47 +0200

    Updated Yeoman generator

--- api/Joplin.d.ts
+++ api/Joplin.d.ts
@@ -7,9 +7,21 @@ import JoplinCommands from './JoplinCommands';
 import JoplinViews from './JoplinViews';
 import JoplinInterop from './JoplinInterop';
 import JoplinSettings from './JoplinSettings';
-import Logger from 'lib/Logger';
+import JoplinContentScripts from './JoplinContentScripts';
 /**
  * This is the main entry point to the Joplin API. You can access various services using the provided accessors.
+ *
+ * **This is a beta API**
+ *
+ * Please note that the plugin API is relatively new and should be considered Beta state. Besides possible bugs, what it means is that there might be necessary breaking changes from one version to the next. Whenever such change is needed, best effort will be done to:
+ *
+ * - Maintain backward compatibility;
+ * - When possible, deprecate features instead of removing them;
+ * - Document breaking changes in the changelog;
+ *
+ * So if you are developing a plugin, please keep an eye on the changelog as everything will be in there with information about how to update your code. There won't be any major API rewrite or architecture changes, but possibly small tweaks like function signature change, type change, etc.
+ *
+ * Eventually, the plugin API will be versioned to make this process smoother.
  */
 export default class Joplin {
     private data_;
@@ -20,10 +32,12 @@ export default class Joplin {
     private views_;
     private interop_;
     private settings_;
-    constructor(logger: Logger, implementation: any, plugin: Plugin, store: any);
+    private contentScripts_;
+    constructor(implementation: any, plugin: Plugin, store: any);
     get data(): JoplinData;
     get plugins(): JoplinPlugins;
     get workspace(): JoplinWorkspace;
+    get contentScripts(): JoplinContentScripts;
     /**
      * @ignore
      *
@@ -35,4 +49,17 @@ export default class Joplin {
     get views(): JoplinViews;
     get interop(): JoplinInterop;
     get settings(): JoplinSettings;
+    /**
+     * It is not possible to bundle native packages with a plugin, because they
+     * need to work cross-platforms. Instead access to certain useful native
+     * packages is provided using this function.
+     *
+     * Currently these packages are available:
+     *
+     * - [sqlite3](https://www.npmjs.com/package/sqlite3)
+     * - [fs-extra](https://www.npmjs.com/package/fs-extra)
+     *
+     * [View the demo plugin](https://github.com/laurent22/joplin/tree/dev/packages/app-cli/tests/support/plugins/nativeModule)
+     */
+    require(_path: string): any;
 }
--- api/JoplinSettings.d.ts
+++ api/JoplinSettings.d.ts
@@ -1,5 +1,12 @@
 import Plugin from '../Plugin';
 import { SettingItem, SettingSection } from './types';
+export interface ChangeEvent {
+    /**
+     * Setting keys that have been changed
+     */
+    keys: string[];
+}
+export declare type ChangeHandler = (event: ChangeEvent)=> void;
 /**
  * This API allows registering new settings and setting sections, as well as getting and setting settings. Once a setting has been registered it will appear in the config screen and be editable by the user.
  *
@@ -7,17 +14,25 @@ import { SettingItem, SettingSection } from './types';
  *
  * Note: Currently this API does **not** provide access to Joplin's built-in settings. This is by design as plugins that modify user settings could give unexpected results
  *
- * [View the demo plugin](https://github.com/laurent22/joplin/tree/dev/CliClient/tests/support/plugins/settings)
+ * [View the demo plugin](https://github.com/laurent22/joplin/tree/dev/packages/app-cli/tests/support/plugins/settings)
  */
 export default class JoplinSettings {
     private plugin_;
     constructor(plugin: Plugin);
+    private get keyPrefix();
     private namespacedKey;
     /**
-     * Registers a new setting. Note that registering a setting item is dynamic and will be gone next time Joplin starts.
+     * Registers new settings.
+     * Note that registering a setting item is dynamic and will be gone next time Joplin starts.
      * What it means is that you need to register the setting every time the plugin starts (for example in the onStart event).
      * The setting value however will be preserved from one launch to the next so there is no risk that it will be lost even if for some
      * reason the plugin fails to start at some point.
+     */
+    registerSettings(settings: Record<string, SettingItem>): Promise<void>;
+    /**
+     * @deprecated Use joplin.settings.registerSettings()
+     *
+     * Registers a new setting.
      */
     registerSetting(key: string, settingItem: SettingItem): Promise<void>;
     /**
@@ -37,7 +52,13 @@ export default class JoplinSettings {
      *
      * The list of available settings is not documented yet, but can be found by looking at the source code:
      *
-     * https://github.com/laurent22/joplin/blob/3539a452a359162c461d2849829d2d42973eab50/ReactNativeClient/lib/models/Setting.ts#L142
+     * https://github.com/laurent22/joplin/blob/dev/packages/lib/models/Setting.ts#L142
      */
     globalValue(key: string): Promise<any>;
+    /**
+     * Called when one or multiple settings of your plugin have been changed.
+     * - For performance reasons, this event is triggered with a delay.
+     * - You will only get events for your own plugin settings.
+     */
+    onChange(handler: ChangeHandler): Promise<void>;
 }
